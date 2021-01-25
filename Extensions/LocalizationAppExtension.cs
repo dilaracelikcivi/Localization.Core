4e657672df8339c2cf940dd658bec6dd11bc51bc
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2021-01-25 13:31:41 +0300

    Localization app extension created.

---
+++ Extensions/LocalizationAppExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Localization.Core.Extensions
+{
+    public static class LocalizationAppExtension
+    {
+        public static void UseLocalization(this IApplicationBuilder app)
+        {
+            var supportedCultures = new[] { "en-US", "tr-TR", "ar", "az"};
+            
+            var localizationOptions = new RequestLocalizationOptions()
+                .SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            app.UseRequestLocalization(localizationOptions);
+        }
+    }
+}
